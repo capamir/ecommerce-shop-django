f79d0376246ecc4734818b1fe36332df6920450c
amirhossein <79211332+capamir@users.noreply.github.com>
2023-04-20 13:26:22 -0400

    migrations and templates settings

--- A/settings.py
+++ A/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # local apps
-    'store.apss.StoreConfig',
+    'store.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'A.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
