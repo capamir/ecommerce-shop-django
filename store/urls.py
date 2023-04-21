ed8530e873a8080a6f2b811334667115450451b8
amirhossein <79211332+capamir@users.noreply.github.com>
2023-04-21 16:58:40 -0400

    configuring urls file and store view

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from . import views
+
+app_name = 'store'
 
 urlpatterns = [
-    
+    path('', views.StoreView.as_view(), name='store'),    
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 
 # Create your views here.
+class StoreView(TemplateView):
+    template_name = "store/store.html"
