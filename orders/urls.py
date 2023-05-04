e96522c7a12bd66745d9f15e633b2eb9eac459e2
amirhossein <79211332+capamir@users.noreply.github.com>
2023-05-04 13:30:06 +0330

    making add to cart function class base view

--- orders/urls.py
+++ orders/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'orders'
 urlpatterns = [
     path('', views.CartSummaryView.as_view(), name='cart_summary'),
-    path('add/', views.cart_add, name='cart_add'),
+    path('add/', views.CartAddView.as_view(), name='cart_add'),
     path('delete/<str:pk>/', views.CartDeleteView.as_view(), name='cart_delete'),
     path('update/<str:pk>/', views.CartUpdateView.as_view(), name='cart_update'),
 
--- orders/views.py
+++ orders/views.py
@@ -11,22 +11,21 @@ class CartSummaryView(TemplateView):
     template_name = 'orders/cart_summary.html'
 
 
-def cart_add(request):
+class CartAddView(View):
+    def setup(self, request, *args, **kwargs):
+        self.cart = Cart(request)
+        return super().setup(request, *args, **kwargs)
 
-    cart = Cart(request)
-
-    if request.POST.get('action') == 'post':
-
+    def post(self, request):
         product_id = request.POST.get('product_id')
         product_qty = int(request.POST.get('product_qty')) 
         product = get_object_or_404(Product, pk=product_id)
-        cart.add(product, product_qty)
+        self.cart.add(product, product_qty)
 
         response = JsonResponse({
             'qty': product_qty
         })
         return response
-        
     
         
 class CartDeleteView(View):
